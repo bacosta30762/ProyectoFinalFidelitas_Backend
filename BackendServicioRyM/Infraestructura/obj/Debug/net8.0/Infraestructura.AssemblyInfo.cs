76b6ad6c421595a73df1ce29cbbc510ea2bf39e2
GabrielHudson-Fide <ghudson90026@ufide.ac.cr>
2024-10-06 20:58:08 -0600

    Login de Brandon
    Login de Brandon

--- BackendServicioRyM/Infraestructura/obj/Debug/net8.0/Infraestructura.AssemblyInfo.cs
+++ BackendServicioRyM/Infraestructura/obj/Debug/net8.0/Infraestructura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e4a13ad5daffe49386b5b3dc9d57c084fa8764")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31cb7ae88655b6b48384136f1fb82c96fe91234")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
