8ad6939705d7b16a2f5b6dfa75494af588570ddd
Brandon2395 <bacosta30762@ufide.ac.cr>
2024-11-15 14:06:04 -0600

    Start up y program.cs

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -1,38 +1,8 @@
-using Aplicacion.Extensiones;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddInfraestructura(builder.Configuration);
-builder.Services.AddAplicacion();
-builder.Services.AddHttpContextAccessor();
-
-// Configurar CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("PermitirFrontend", builder =>
-    {
-        builder.WithOrigins("http://localhost:3000", //local
-            "http://localhost:3001", //local
-            "https://bacosta30762.github.io") //Github Origen del frontend
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials(); // Si estás manejando cookies o autenticación
-    });
-});
-
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
-    });
+// Agregar configuración del Startup
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
@@ -42,21 +12,7 @@ using (var scope = app.Services.CreateScope())
     await ServicioSeeder.SeedServiciosAsync(scope.ServiceProvider);
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("PermitirFrontend"); // Aplicar política de CORS
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
+// Configuración del pipeline
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ Presentacion/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Aplicacion.Extensiones;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    // Configuración de servicios
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Agregar servicios al contenedor
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddInfraestructura(Configuration);
+        services.AddAplicacion();
+        services.AddHttpContextAccessor();
+
+        // Configurar CORS
+        services.AddCors(options =>
+        {
+            options.AddPolicy("PermitirFrontend", builder =>
+            {
+                builder.WithOrigins("http://localhost:3000", //local
+                    "http://localhost:3001", //local
+                    "https://bacosta30762.github.io") //GitHub Origen del frontend
+                       .AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .AllowCredentials(); // Si estás manejando cookies o autenticación
+            });
+        });
+
+        // Configuración de JSON
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+            });
+    }
+
+    // Configuración del pipeline HTTP
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors("PermitirFrontend"); // Aplicar política de CORS
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+}
