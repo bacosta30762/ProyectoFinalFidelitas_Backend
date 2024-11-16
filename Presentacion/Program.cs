035b96323f1da7b697c9791a21fe30b238ee00ca
Brandon2395 <bacosta30762@ufide.ac.cr>
2024-11-16 14:55:21 -0600

    Update Program.cs

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Configurar el host para escuchar"
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
+//builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 // Agregar configuración del Startup
 var startup = new Startup(builder.Configuration);
