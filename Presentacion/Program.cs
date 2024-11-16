09774a52c9ece143447cd70283d139e0e82d4e52
Brandon2395 <bacosta30762@ufide.ac.cr>
2024-11-16 14:15:09 -0600

    update program
    conexion a la bd de azure y puerto 80

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar el host para escuchar en "http://0.0.0.0:8080"
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
 // Agregar configuración del Startup
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
